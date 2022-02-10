a565bd5dcc5c2f8dc354568ad2ddda1195cdc147
Tony Pagaduan <Elte156@gmail.com>
2022-02-09 23:14:47 -0500

    Added ngIf: a structural directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
   username = '';
 
   constructor() {
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
